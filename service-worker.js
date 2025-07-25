88de5fe0f88d518e7bcc5aeabfd66bb482c9891f
Tristan14123 <tristan-v@hotmail.fr>
2025-07-25 16:40:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('menu-ai-cache-v1').then(cache => {
       return cache.addAll([
-        '/',
+        '/Menu_AI',
         '/Menu_AI/index.html',
         '/Menu_AI/manifest.json',
         // Ajoute ici les autres fichiers à mettre en cache
