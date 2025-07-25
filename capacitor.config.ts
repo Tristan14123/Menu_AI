e052c155f947b7e15cc6e78a4329e8826aec011a
Tristan14123 <tristan-v@hotmail.fr>
2025-07-25 15:29:13 +0200

    Added capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cuisineai.app',
+  appName: 'CuisineAI',
+  webDir: 'dist'
+};
+
+export default config;
