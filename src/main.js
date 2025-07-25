f25a14cd357e018ca72f2eab6ee1dc84b472c8de
Tristan14123 <tristan-v@hotmail.fr>
2025-07-25 16:26:32 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/Menu_AI/service-worker.js')
       .then(reg => console.log('Service Worker enregistré', reg))
       .catch(err => console.error('Erreur Service Worker', err));
   });
